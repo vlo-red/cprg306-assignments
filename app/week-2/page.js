7cea86a768e2274e8b8de169dc942c2f7447232d
vlo-red <jared.lopez@edu.sait.ca>
2026-01-22 18:16:17 -0700

    Configured both page.js to link root page with week-2 page

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,11 @@
-export default function Page() {
+import StudentInfo from './StudentInfo.js';
+
+export default function Week2Page() {
   return (
     <main>
       <h1>Shopping List</h1>
+
+      <StudentInfo />
     </main>
   );
 }
