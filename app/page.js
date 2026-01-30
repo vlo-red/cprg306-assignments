63a03e4a3e81b9c41d964fa0f7cceb8e2f9e53cd
vlo-red <jared.lopez@edu.sait.ca>
2026-01-30 11:53:47 -0700

    Added Week-3 Assignment Link to home page

--- app/page.js
+++ app/page.js
@@ -3,10 +3,14 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main>
-      <h1>CPRG 306-C: Week 2 Assignment</h1>
+      <h1>CPRG 306-C: Weekly Assignments</h1>
       <p>
         Go to Week-2 Assignment:{' '}
         <Link href="/week-2">Week-2 Page</Link>
+      </p>
+      <p>
+        Go to Week-3 Assignment:{' '}
+        <Link href="/week-3">Week-3 Page</Link>
       </p>
     </main>
   );
