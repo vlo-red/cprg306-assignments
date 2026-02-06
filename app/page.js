18659a6c1d8f003a2c3e6d455050f37c35474b3c
vlo-red <jared.lopez@edu.sait.ca>
2026-02-05 18:24:53 -0700

    Put links in a list for home page

--- app/page.js
+++ app/page.js
@@ -4,14 +4,20 @@ export default function HomePage() {
   return (
     <main>
       <h1>CPRG 306-C: Weekly Assignments</h1>
-      <p>
-        Go to Week-2 Assignment:{' '}
-        <Link href="/week-2">Week-2 Page</Link>
-      </p>
-      <p>
-        Go to Week-3 Assignment:{' '}
-        <Link href="/week-3">Week-3 Page</Link>
-      </p>
+      <ul>
+        <li>
+          Go to Week-2 Assignment:{' '}
+          <Link href="/week-2">Week-2 Page</Link>
+        </li>
+        <li>
+          Go to Week-3 Assignment:{' '}
+          <Link href="/week-3">Week-3 Page</Link>
+        </li>
+        <li>
+          Go to Week-4 Assignment:{' '}
+          <Link href="/week-4">Week-4 Page</Link>
+        </li>
+      </ul>
     </main>
   );
 }
